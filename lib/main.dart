28df8134c19550bf8f07fb113aa5d1b753073dcf
EneRgYCZ <catalin.zaharia2013@gmail.com>
2022-01-21 18:30:47 +0100

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:auth_demo/routes.dart';
+import 'package:flutter/material.dart';
+
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return const Text('error');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            routes: appRoutes,
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return const Text('loading');
+      },
+    );
+  }
+}
